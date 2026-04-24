6baf530a54c812042615dcf62902865b9713c038
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-04-25 00:26:43 +0900

    feat: Support input history

--- addons/urakata/src/repl/repl.gd
+++ addons/urakata/src/repl/repl.gd
@@ -3,6 +3,13 @@ extends HBoxContainer
 
 @export var modes : Array[UrakataMode]
 var current_mode : UrakataMode
+var input_histories : Array[InputHistory]
+var history_back_idx := 0
+
+
+class InputHistory:
+	var mode : UrakataMode
+	var input : String
 
 
 func _ready() -> void:
@@ -17,6 +24,7 @@ func submit() -> void:
 	if not text: return
 	print_rich('%s %s' % [current_mode.prompt_prefix(), text])
 	clear_prompt()
+	_append_input_history(text)
 	print_rich('=> %s\n' % current_mode.perform(text))
 
 
@@ -38,6 +46,16 @@ func remove_following_text() -> void:
 		%Prompt.get_line(idx).length())
 
 
+func history_back() -> void:
+	_set_input_from_history(1)
+	call_deferred('_move_prompt_caret_last')
+
+
+func history_forward() -> void:
+	_set_input_from_history(-1)
+	call_deferred('_move_prompt_caret_last')
+
+
 func change_mode(mode: UrakataMode) -> void:
 	current_mode = mode
 	%Indicator.text = mode.indicator()
@@ -54,6 +72,35 @@ func _set_syntax_highligher() -> void:
 		$Prompt.syntax_highlighter = GDScriptSyntaxHighlighter
 
 
+func _append_input_history(text: String) -> void:
+	var record = InputHistory.new()
+	record.mode = current_mode
+	record.input = text
+	input_histories.push_back(record)
+
+
+func _set_input_from_history(direction: int) -> void:
+	history_back_idx += direction
+	var count = input_histories.size()
+	if history_back_idx < 1:
+		history_back_idx = 0
+		%Prompt.text = ''
+	elif count < history_back_idx:
+		history_back_idx = count + 1
+		%Prompt.text = ''
+	else:
+		var record = input_histories.get(count - history_back_idx)
+		change_mode(record.mode)
+		%Prompt.text = record.input
+
+
+func _move_prompt_caret_last() -> void:
+	var l_idx = %Prompt.get_line_count()
+	var c_idx = %Prompt.get_line(l_idx - 1).length()
+	%Prompt.set_caret_line(l_idx)
+	%Prompt.set_caret_column(c_idx)
+
+
 func _prompt_input(event: InputEvent) -> void:
 	if not (event is InputEventKey and event.pressed): return
 	var k := event as InputEventKey
@@ -67,6 +114,12 @@ func _prompt_input(event: InputEvent) -> void:
 			if k.ctrl_pressed: Urakata.clear_output()
 		KEY_U:
 			if k.ctrl_pressed: clear_prompt()
+		KEY_UP:
+			if %Prompt.get_line_count() == 1:
+				history_back()
+		KEY_DOWN:
+			if %Prompt.get_line_count() == 1:
+				history_forward()
 		_:
 			if not k.ctrl_pressed and %Prompt.text.is_empty():
 				if k.keycode == KEY_BACKSPACE:
--- test/repl/test_repl.gd
+++ test/repl/test_repl.gd
@@ -6,7 +6,44 @@ var scene = null
 
 func before_each():
 	scene = Scene.instantiate()
+	scene.change_mode(scene.modes[0])
+
+
+func get_prompt_text() -> String:
+	return scene.get_node('%Prompt').text
 
 
 func test_ready():
 	assert_not_null(scene)
+	assert_ne(scene.modes.size(), 0)
+	assert_not_null(scene.current_mode)
+
+
+func test_append_input_history():
+	assert_eq(scene.input_histories.size(), 0)
+	scene._append_input_history('1')
+	assert_eq(scene.input_histories.size(), 1)
+
+
+func test_set_input_from_history_if_empty():
+	scene._set_input_from_history(-1)
+	assert_eq(get_prompt_text(), '')
+	scene._set_input_from_history(1)
+	assert_eq(get_prompt_text(), '')
+
+
+func test_set_input_from_history_if_any():
+	scene._append_input_history('1')
+	scene._append_input_history('2')
+	scene._set_input_from_history(1)
+	assert_eq(get_prompt_text(), '2')
+	scene._set_input_from_history(1)
+	assert_eq(get_prompt_text(), '1')
+	scene._set_input_from_history(1)
+	assert_eq(get_prompt_text(), '')
+	scene._set_input_from_history(-1)
+	assert_eq(get_prompt_text(), '1')
+	scene._set_input_from_history(-1)
+	assert_eq(get_prompt_text(), '2')
+	scene._set_input_from_history(-1)
+	assert_eq(get_prompt_text(), '')
